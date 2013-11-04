5350ffaf119e8636ac0971fdf846cd4318ad2526
Jakub Matys <jakub.matys@nodus.cz>
2013-11-04 16:58:44 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
               'log2sentry.raven',
               'log2sentry.raven.serializer'],
     scripts=['scripts/log2sentry-prepare'],
-    version='0.3',
+    version='0.4',
     author='Jakub Matys',
     author_email='matys.jakub@gmail.com',
     url='https://github.com/jakm/log2sentry',
